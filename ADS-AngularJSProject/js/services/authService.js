0ca8cf7c84906372f971416ded9fb5042057d768
plami <plamena.y.nikolova@gmail.com>
2015-01-09 12:58:27 -0800

    adding adsServices

--- ADS-AngularJSProject/js/services/authService.js
+++ ADS-AngularJSProject/js/services/authService.js
@@ -1,3 +1,63 @@
 /**
  * Created by plamena on 1/8/2015.
  */
+
+'use strict';
+
+app.factory('authService',
+    function ($http, baseServiceUrl) {
+        return {
+            login: function(userData, success, error) {
+                var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/user/login',
+                    data: userData
+                };
+                $http(request).success(function(data){
+                   sessionStorage['currentUser'] = JSON.stringify(data);
+                    success(data);
+                }).error(error);
+            },
+
+            register: function(userData, success, error) {
+                var request = {
+                    method: 'POST',
+                    url: baseServiceUrl + '/api/user/register',
+                    data: userData
+                };
+                $http(request).success(function(data){
+                    sessionStorage['currentUser'] = JSON.stringify(data);
+                    success(data);
+                }).error(error)
+            },
+
+            logout: function() {
+                delete sessionStorage['CurrentUser'];
+            },
+
+            getCurrentUser : function() {
+                
+            },
+
+            isAnonymous : function() {
+                // TODO
+            },
+
+            isLoggedIn : function() {
+                // TODO
+            },
+
+            isNormalUser : function() {
+                // TODO
+            },
+
+            isAdmin : function() {
+                // TODO
+            },
+
+            getAuthHeaders : function() {
+                // TODO
+            }
+        }
+    }
+);
